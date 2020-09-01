ce734a03e4dd0e2575da19445b2a21f10e82f36e
satishgupta07 <satishgupta.cse01@gmail.com>
2020-09-01 22:09:01 +0530

    Added file selector and uploading the file using XHR
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const dropZone = document.querySelector(".drop-zone");
+const fileInput = document.querySelector("#fileInput");
+const browseBtn = document.querySelector("#browseBtn");
+
+const host = "https://innshare.herokuapp.com/"
+const uploadURL = `${host}api/files`
 
 dropZone.addEventListener("dragover", (e) => {
     e.preventDefault();
@@ -16,4 +21,38 @@ dropZone.addEventListener("dragover", (e) => {
   dropZone.addEventListener("drop", (e) => {
     e.preventDefault();
       dropZone.classList.remove("dragged")
+
+      const files = e.dataTransfer.files
+      console.log(files)
+      if(files.length){
+        fileInput.files = files;
+        uploadFile()
+      }
+   
   })
+
+  fileInput.addEventListener("change", () => {
+      uploadFile()
+  })
+
+  browseBtn.addEventListener("click", () => {
+      fileInput.click()
+  })
+
+  const uploadFile = () => {
+
+      const file = fileInput.files[0]
+      const formData = new FormData()
+      formData.append("myfile", file)
+
+      const xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = () => {
+          if(xhr.readyState === XMLHttpRequest.DONE) {
+              console.log(xhr.response)
+          }
+      }
+
+      xhr.open("POST", uploadURL)
+      xhr.send(formData)
+
+  }
